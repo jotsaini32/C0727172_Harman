8bbc5c1db9d196e49a55f5f7126c801995f7cd83
visualstudio <visualstudio@122-PC34>
2019-01-25 21:13:22 -0500

    Second commit

--- C0727172_Harman/Program.cs
+++ C0727172_Harman/Program.cs
@@ -10,7 +10,45 @@ namespace C0727172_Harman
     {
         static void Main(string[] args)
         {
-
         }
     }
+    class Countrysider
+    {
+
+        Village Alst;
+        Village Maeland;
+        Village Schenig;
+
+        public void InitializeMap()
+        {
+
+
+            Alst = new Village();
+            Maeland = new Village();
+            Maeland.isAstrildeHere = true;
+            Schenig = new Village();
+
+            Alst.NextVillage = Maeland;
+            Maeland.NextVillage = Schenig;
+            Schenig.NextVillage = Alst;
+        }
+
+        public void WalkAround()
+        {
+            Village InitializeVillage = this.Maeland;
+        }
+    }
+
+    class Village
+    {
+        public Village PreviousVillage;
+        public Village NextVillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
+        public Village() { }
+
+
+    }
 }
+
+
